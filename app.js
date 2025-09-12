b9eb328ad007b8983d6e91df3e4e27940d361fb5
Ferney Suarez <ferneysua123@gmail.com>
2025-09-11 23:29:58 -0300

    Funcionalidad crear lista de amigos con solo verificación de campo vacío

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    let inputAmigo = document.getElementById("amigo")
+    let amigo = inputAmigo.value;
+    //console.log(amigo);
+    if(amigo === ""){
+        alert("Por Favor, ingrese el nombre de un amigo.");
+        return;
+    }
+    amigos.push(amigo);
+    console.log(amigos);
+}
